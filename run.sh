bb72544beb55785c6810087aac10b24567b633e4
bhbhjs <bharkbhoom.jae@student.mahidol.edu>
2021-01-31 16:37:38 +0700

    test

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 # timer=$SECONDS
 
-# python3 mtbtyper.py -h
-python3 mtbtyper.py vcf -o lineage #--final_prediction
+# python3 mtbtyper.py 
+python3 mtbtyper.py vcf -o lineage
 
 # timer=$(($SECONDS-timer))
 # printf "Time used: %02d:%02d:%02d\n" "$((timer/3600))" "$((timer/60%60))" "$((timer%60))"
