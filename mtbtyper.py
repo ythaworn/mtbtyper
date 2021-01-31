724172a1d81e2ee94e49fef9eadc0643b696b764
ythaworn <yuttapong.thawornwattana@gmail.com>
2021-01-31 16:40:29 +0700

    update copyright

--- mtbtyper.py
+++ mtbtyper.py
@@ -2,16 +2,19 @@
 #
 # mtbtyper
 #
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2020
+# Yuttapong Thawornwattana & Bharkbhoom Jaemsai
+#
 # Requirements:
 # (1) python packages:
 #     - numpy
 #     - pandas
 #     - scikit-allel
 # (2) SNP schemes (csv files) in snpdb/ directory
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 
 import os
